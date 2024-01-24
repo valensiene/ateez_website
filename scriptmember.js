a56d50e48c46c3a631d58ed6f86b97a154f246aa
valensiene <virginia.cvat@gmail.com>
2024-01-24 09:26:08 +0700

    first commit ATEEZ WEBSITE

---
+++ scriptmember.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+  }
+
+  // Event listeners for next and previous buttons
+  document.querySelector(".prev").addEventListener("click", function() {
+    plusSlides(-1);
+  });
+
+  document.querySelector(".next").addEventListener("click", function() {
+    plusSlides(1);
+  });
+
+});
